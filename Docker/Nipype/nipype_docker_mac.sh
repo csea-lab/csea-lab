a541a5f7d545dbc88c637e1ff888f38f57dd38d6
veliebm <67563877+veliebm@users.noreply.github.com>
2020-07-22 10:26:13 -0400

    Rename config to show it's for mac

--- Docker/Nipype/nipype_docker_mac.sh
+++ Docker/Nipype/nipype_docker_mac.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-source config.txt
+source nipype_docker_mac_config.txt
 
 docker run --interactive --tty --rm --user $user --name $name --workdir $workdir --volume $mount -p $p $image bash
