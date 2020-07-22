60eac876f61b2403fbd0e16bc109ab5fdbce7fc9
veliebm <67563877+veliebm@users.noreply.github.com>
2020-07-22 11:11:58 -0400

    Source a shell script instead of a text file

--- Docker/Nipype/nipype_docker_mac.sh
+++ Docker/Nipype/nipype_docker_mac.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-source nipype_docker_mac_config.txt
+source config_nipype_docker_mac.sh
 
 docker run --interactive --tty --rm --user $user --name $name --workdir $workdir --volume $mount -p $p $image bash
