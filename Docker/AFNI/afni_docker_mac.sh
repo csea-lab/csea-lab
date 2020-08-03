55b80ec57c4f9ce24fcbe6fb60ec345955679ac8
veliebm <67563877+veliebm@users.noreply.github.com>
2020-08-03 18:53:59 -0400

    Add GUI toggle and readonly dir

--- Docker/AFNI/afni_docker_mac.sh
+++ Docker/AFNI/afni_docker_mac.sh
@@ -1,17 +1,33 @@
 #! /usr/bin/env bash
 
-# Image to run inside the container.
+#Image to run inside the container.
 image="afni/afni"
 # Name of the container.
 name="afni"
-# Directory to mount to the container.
-source="/Volumes/volume"
-# Sets the location of the mounted directory inside the container.
-destination="/volume"
+# Writeable directory to mount to the container.
+write_source="/Volumes/volume/"
+# Sets the location of the writeable directory within the container.
+write_destination="/write_host/"
+# Read-only directory to mount to the container.
+read_source=$userprofile
+# Sets the location of the read-only directory within the container.
+read_destination="/read_host/"
 # Sets the working directory within the container.
-workdir="/volume"
+workdir="/write_host/"
 # Sets the port the container runs on.
 p="8889:8889"
+# Sets whether the AFNI GUI is enabled.
+enableGUI=False
+# Sets the settings to make X Quartz work
+GUI="DISPLAY=docker.for.mac.host.internal:0"
 
-
-docker run --interactive --tty --rm --name $name --volume $mount --workdir $workdir -p $p --env DISPLAY=docker.for.mac.host.internal:0 $image bash
+if [ $enableGUI = True ]
+then
+    echo "The GUI is enabled, which can make the container buggy"
+    echo "Please run the container without the GUI unless you're using the AFNI viewer"
+    docker run --interactive --tty --rm --name $name --volume $write_mount --volume $read_mount --workdir $workdir -p $p --env $GUI $image bash
+else
+    echo "The GUI is disabled"
+    echo "To enable the GUI, launch this script with the flag -enableGUI"
+    docker run --interactive --tty --rm --name $name --volume $write_mount --volume $read_mount --workdir $workdir -p $p $image bash
+fi
