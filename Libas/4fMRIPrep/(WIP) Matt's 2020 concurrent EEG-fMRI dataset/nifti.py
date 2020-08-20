c1d8dacc9be92c6755fabab373d4da6ec999f43d
Ben Velie <67563877+veliebm@users.noreply.github.com>
2020-08-20 09:02:22 -0400

    Document the class

--- Libas/4fMRIPrep/(WIP) Matt's 2020 concurrent EEG-fMRI dataset/nifti.py
+++ Libas/4fMRIPrep/(WIP) Matt's 2020 concurrent EEG-fMRI dataset/nifti.py
@@ -9,10 +9,30 @@ import pathlib
 import nibabel
 
 class Nifti():
+    """
+    Class to organize data obtained from NIFTI files.
+
+    Parameters
+    ----------
+    input_path : str or Path
+
+    ...
+
+    Attributes
+    ----------
+    path : Path
+        The path of the NIFTI file.
+    file : nibabel Nifti1Image
+        The NIFTI file loaded with nibabel.
+    header : nibabel Nifti1Header
+        The header of the NIFTI file.
+    volume_count : int
+        The number of volumes in the NIFTI file.
+    """
 
     def __init__(self, input_path):
         
         self.path = pathlib.Path(input_path)
         self.file = nibabel.load(input_path)
         self.header = self.file.header
-        self.slice_count = self.header["slice_end"] - self.header["slice_start"] + 1
+        self.volume_count = self.header["slice_end"] - self.header["slice_start"] + 1
