a8e69ff73ed5849afafa831bbac8a287b19d59f5
Ben Velie <67563877+veliebm@users.noreply.github.com>
2020-09-15 14:25:30 -0400

    Simplify exported vars; make script runnable

--- lib_ben/contrascan/submitjob.sh
+++ lib_ben/contrascan/submitjob.sh
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1					# Run a single task		
 #SBATCH --cpus-per-task=2				# Number of CPU cores per task
 #SBATCH --mem=8gb						# Job memory request
-#SBATCH --time=0-00:05:00				# Walltime in hh:mm:ss or d-hh:mm:ss
+#SBATCH --time=2-00:00:00				# Walltime in hh:mm:ss or d-hh:mm:ss
 # Outputs ----------------------------------
 #SBATCH --mail-type=ALL					# Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=veliebm@ufl.edu		      # Where to send mail	
@@ -18,37 +18,31 @@
 pwd; hostname; date					# Useful things we'll want to see in the log
 # ------------------------------------------
 
-BIDS_DIR="$STUDY"
-DERIVS_DIR="derivatives/fmriprep"
-LOCAL_FREESURFER_DIR="$STUDY/derivatives/freesurfer"
+export HOME="/blue/akeil/veliebm"
+BIDS_DIR="$HOME/Files/contrascan/BIDS3"
+DERIVS_DIR="$BIDS_DIR/derivatives"
+LOCAL_FREESURFER_DIR="$DERIVS_DIR/freesurfer"
+
+# Set subject equal to first argument after this script on the command line.
+subject=$1
 
 # Make sure FS_LICENSE is defined in the container.
-export SINGULARITYENV_FS_LICENSE=/home/veliebm/.licenses/freesurfer.txt
+export SINGULARITYENV_FS_LICENSE="$HOME/Files/.licenses/freesurfer.txt"
 
-# Prepare some writeable bind-mount points.
-TEMPLATEFLOW_HOST_HOME=$HOME/.cache/templateflow
-FMRIPREP_HOST_CACHE=$HOME/.cache/fmriprep
-mkdir -p "${TEMPLATEFLOW_HOST_HOME}"
-mkdir -p "${FMRIPREP_HOST_CACHE}"
-
-# Prepare derivatives folder
-mkdir -p "${BIDS_DIR}"/${DERIVS_DIR}
+# Prepare derivatives folder.
+mkdir -p "${BIDS_DIR}/${DERIVS_DIR}"
 mkdir -p "${LOCAL_FREESURFER_DIR}"
 
-# Designate a templateflow bind-mount point
-export SINGULARITYENV_TEMPLATEFLOW_HOME="/templateflow"
-SINGULARITY_CMD="singularity run --cleanenv -B $BIDS_DIR:/data -B ${TEMPLATEFLOW_HOST_HOME}:${SINGULARITYENV_TEMPLATEFLOW_HOME} -B $L_SCRATCH:/work -B ${LOCAL_FREESURFER_DIR}:/fsdir /blue/akeil/veliebm/Files/Images/fmriprep_latest.sif"
+# Compose command to start singularity.
+SINGULARITY_CMD="singularity run --home $HOME --cleanenv $HOME/Files/Images/fmriprep-20.1.2.simg"
 
-# Parse the participants.tsv file and extract one subject ID from the line corresponding to this SLURM task.
-subject=$( sed -n -E "$((${SLURM_ARRAY_TASK_ID} + 1))s/sub-(\S*)\>.*/\1/gp" "${BIDS_DIR}"/participants.tsv )
+# Remove IsRunning files from FreeSurfer.
+find "${LOCAL_FREESURFER_DIR}/sub-$subject"/ -name "*IsRunning*" -type f -delete
 
-# Remove IsRunning files from FreeSurfer
-find "${LOCAL_FREESURFER_DIR}"/sub-"$subject"/ -name "*IsRunning*" -type f -delete
+# Compose the command line.
+cmd="${SINGULARITY_CMD} $BIDS_DIR $DERIVS_DIR participant --participant-label $subject -vv --write-graph --resource-monitor --n-cpus 2 --mem 8"
 
-# Compose the command line
-cmd="${SINGULARITY_CMD} /data /data/${DERIVS_DIR} participant --participant-label $subject -w /work/ -vv --output-spaces MNI152NLin2009cAsym:res-2 anat fsnative fsaverage5 --use-aroma --fs-subjects-dir /fsdir"
-
-# Setup done, run the command
+# Setup done, run the command.
 echo Running task "${SLURM_ARRAY_TASK_ID}"
 echo Commandline: "$cmd"
 eval "$cmd"
