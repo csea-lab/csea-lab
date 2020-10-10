1d51a65f854a79f49961e75d1a43bd305792b571
Ben Velie <67563877+veliebm@users.noreply.github.com>
2020-10-10 12:41:33 -0400

    Update docstrings

--- lib-ben/dataset-contrascan/dat.py
+++ lib-ben/dataset-contrascan/dat.py
@@ -5,6 +5,7 @@ Class to organize and extract data from a .dat file.
 
 Created 8/20/2020 by Benjamin Velie.
 veliebm@gmail.com
+
 """
 
 import pathlib
@@ -14,15 +15,18 @@ class Dat():
     """
     Class to organize and extract data from a NIFTI file.
 
+
     Parameters
     ----------
     input_path : str or Path
         Path to the .dat file to be processed.
 
+
     Attributes
     ----------
     path : Path
         Path to the .dat file.
+
     """
 
     def __init__(self, input_path):
@@ -35,6 +39,7 @@ class Dat():
         Returns a list of durations extracted from the .dat file.
 
         Automatically converts times into seconds.
+
         """
 
         raw_durations = self._raw_durations()
@@ -45,6 +50,7 @@ class Dat():
     def line_list(self) -> list:
         """
         Returns the .dat file as a list of lines.
+
         """
 
         return self.path.read_text().splitlines()
@@ -53,6 +59,7 @@ class Dat():
     def average_duration(self) -> float:
         """
         Returns the average stimulus duration recorded in the .dat file.
+
         """
 
         return sum(self.durations()) / len(self.durations())
@@ -63,6 +70,7 @@ class Dat():
         Converts a list of raw durations into clean durations.
 
         Each raw duration is converted from scientific notation to a float.
+
         """
 
         clean_durations = []
@@ -79,6 +87,7 @@ class Dat():
         Returns a list of all raw durations from the .dat file.
 
         Note that the durations must be further cleaned.
+
         """
 
         raw_durations = []
