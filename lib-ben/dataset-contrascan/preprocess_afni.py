d0f41191b4c62b3e29a2a04bc1656c8b89decf9e
Ben Velie <67563877+veliebm@users.noreply.github.com>
2020-10-18 18:00:40 -0400

    Add 3dROIstats

--- lib-ben/dataset-contrascan/preprocess_afni.py
+++ lib-ben/dataset-contrascan/preprocess_afni.py
@@ -21,6 +21,7 @@ from datetime import datetime
 import argparse
 from pathlib import Path
 import json
+import re
 
 
 # Import some friendly and nice CSEA custom modules. -------------------
@@ -73,6 +74,7 @@ class Preprocess():
         self.results["tshift"] = self.tshift()
         self.results["volreg"] = self.volreg()
         self.results["merge"] = self.merge()
+        self.results["roistats"] = self.roistats()
 
 
         # Record end time and write our report. --------------------------
@@ -337,7 +339,7 @@ class Preprocess():
         args = f"""
             -1blur_fwhm 5.0
             -doall
-            -prefix sub-{subject_id}_smoothed
+            -prefix sub-{subject_id}_func_smoothed
             {self.results["volreg"].outfile}
         """.split()
 
@@ -352,6 +354,64 @@ class Preprocess():
 
         # Store path to outfile as an attribute of the results. Return results. ----------------------------
         results.outfile = the_path_that_matches("*_smoothed+orig.HEAD", in_directory=results.working_directory)
+        return results
+
+
+    def roistats(self):
+        """
+        Get the mean for each of n slices for each timepoint to check on residual motion.
+
+        TODO: Explain what that summary actually means??
+
+        Also, this wrapper is a little funny. 3dROIstats doesn't create a file - it prints a matrix
+        to stdout. Because I don't want to rewrite afni.py to be able to write said matrix directly 
+        to disk, we'll collect the stdout here and write it within this wrapper.
+
+        Wraps 3dROIstats.
+
+        AFNI command info: https://afni.nimh.nih.gov/pub/dist/doc/htmldoc/programs/3dROIstats_sphx.html#ahelp-3droistats
+
+
+        Returns
+        -------
+        AFNI object
+            Stores information about the program.
+
+        """
+
+        # Prepare the arguments we want to pass to the program. ---------------------
+        # We append 3dcalc separately to the args so we don't accidentally split it.
+        mask_arg = ["-mask", f"3dcalc(-a {self.results['resample'].outfile} -expr a*(k+1) -datum short -nscale)"]
+        other_args = f"""
+        -quiet
+        {self.results["volreg"].outfile}
+        """.split()
+
+
+        # Run program and store results. -----------------------
+        working_directory_of_program = self.dirs["output"]/"3dROIstats"
+        results = AFNI(
+            program="3dROIstats",
+            args=mask_arg + other_args,
+            working_directory=working_directory_of_program
+        )
+
+
+        # Write the matrix to disk. -------------------------
+        # A line of stdout is part of the matrix if it exclusively contains numbers, tabs, and decimal signs.
+        def is_part_of_matrix(string, contains_wrong_characters=re.compile(r'[^\t0-9.]').search):
+            return not bool(contains_wrong_characters(string))
+
+        matrix_lines = [line for line in results.stdout_and_stderr.splitlines() if is_part_of_matrix(line)]
+
+        matrix_path = working_directory_of_program / f"sub-{self.subject_id}_sliceaverage.1D"
+
+        with open(matrix_path, "w") as file:
+            file.writelines(matrix_lines)
+
+
+        # Store path to outfile as an attribute of the results. Return results. ----------------------------
+        results.outfile = the_path_that_matches("*_sliceaverage.1D", in_directory=results.working_directory)
         return results
 
 
