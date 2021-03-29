151746d11e64e9bd46b9b9bf538a3aff479de2f0
veliebm <veliebm@gmail.com>
2021-03-29 13:39:30 -0400

    Totally revamp to use cached properties

--- benlib/bidspath.py
+++ benlib/bidspath.py
@@ -6,9 +6,10 @@ veliebm@gmail.com
 """
 
 from pathlib import Path
-from dataclasses import dataclass, field
-from typing import Dict
+from dataclasses import dataclass
+from typing import Dict, Union
 import re
+from functools import cached_property
 
 
 @dataclass
@@ -18,23 +19,15 @@ class BIDSpath:
     ----------
     path: Path
         Path to something named according to BIDS.
-    dictionary: Dict
-        Stores key-value pairs from the file name.
-    is_afni: bool
-        True if path leads to something named like an AFNI file.
     """
     _path: Path
-    dictionary: Dict[str, str] = field(init=False)
-    is_afni: bool = field(init=False) 
 
 
     def __post_init__(self) -> None:
         """
         This method calls after the __init__ method.
         """
-        self.path = Path(self.path).absolute()
-        self._update_attributes()
-
+        self._path = Path(self.path).absolute()
 
     def __getitem__(self, key: str) -> str:
         """
@@ -42,31 +35,31 @@ class BIDSpath:
         """
         return self.dictionary[key]
 
-
     @property
     def path(self) -> Path:
         return self._path
 
     @path.setter
-    def path(self, _path: Path) -> None:
-        self._path = Path(_path).absolute()
-        self._update_attributes()
+    def path(self, _) -> None:
+        """
+        Make it impossible to change self.path.
+        """
+        pass
 
-
-    def get_prefix(self) -> str:
+    @cached_property
+    def prefix(self) -> str:
         """
         Returns the stem. If our path is an AFNI path, removes the view.
         """
         prefix = self.path.stem
 
         if self.is_afni:
-            view = self.get_view()
-            prefix = re.search(pattern=f"(.+)\\+{view}$", string=prefix).group(1)
+            prefix = re.search(pattern=f"(.+)\\+{self.view}$", string=prefix).group(1)
         
         return prefix
 
-
-    def get_view(self) -> str:
+    @cached_property
+    def view(self) -> str:
         """
         If we have an AFNI path, then return its view, else return None.
         """
@@ -78,35 +71,13 @@ class BIDSpath:
 
         return view
 
-
-    def _reset_attributes_to_default(self) -> None:
-        """
-        Sets our attributes to their default values.
-        """
-        self.dictionary = {}
-        self.is_afni = False
-
-
-    def _update_attributes(self) -> None:
-        """
-        Update our attributes.
-        """
-        self._reset_attributes_to_default()
-
-        # Determine whether this is an AFNI path.
-        stem = self.path.stem
-        if re.search(pattern=r"(\+tlrc|\+orig)$", string=stem):
-            self.is_afni = True
-        
-        # Parse dictionary of the path.
-        self._parse_dictionary()
-
-
-    def _parse_dictionary(self) -> None:
+    @cached_property
+    def dictionary(self) -> Dict[str, Union[str, bool]]:
         """
         Parses keys and values from the name of self.path. Keys without values are assigned True.
         """
-        prefix = self.get_prefix()
+        prefix = self.prefix
+        dictionary = {}
 
         segments = prefix.split("_")
         for segment in segments:
@@ -115,4 +86,19 @@ class BIDSpath:
             except ValueError:
                 key, value = segment, True
 
-            self.dictionary[key] = value
+            dictionary[key] = value
+        
+        return dictionary
+
+    @cached_property
+    def is_afni(self) -> bool:
+        """
+        Determine whether this is an AFNI path.
+        """
+        is_afni = False
+
+        stem = self.path.stem
+        if re.search(pattern=r"(\+tlrc|\+orig)$", string=stem):
+            is_afni = True
+
+        return is_afni
