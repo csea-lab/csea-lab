200c2722fc94c042701748fbfeb72c19f920ef37
veliebm <veliebm@gmail.com>
2021-04-08 12:32:07 -0400

    Implement even better decorator with arguments

--- benlib/neurologging.py
+++ benlib/neurologging.py
@@ -12,47 +12,52 @@ import json
 from pathlib import Path
 
 
-def logged(function: FunctionType) -> FunctionType:
+def logged(log_path) -> FunctionType:
     """
-    Wrapper to log a Python function.
+    Decorator to log a Python function.
 
     Parameters
     ----------
-    log_path: PathLike="log.json"
+    log_path: PathLike
         Where to dump the log.
+
+    Learn about decorators: https://stackoverflow.com/questions/739654/how-to-make-function-decorators-and-chain-them-together/1594484#1594484
     """
-    @wraps(function)
-    def wrapper(log_path: PathLike, *args, **kwargs):
-
-        # What to do before executing the function.
-        start_time = datetime.now()
-        potential_exception = None
-
-        # Try to execute the function.
-        try:
-            result = function(*args, **kwargs)
-            return result
-        except Exception as e:
-            potential_exception = e
-
-        # What to do after executing the function.
-        finally:
-            end_time = datetime.now()
-            writing_dictionary = {
-                "Start time": start_time,
-                "End time": end_time,
-                "Working directory": Path().absolute(),
-                "Module": function.__module__,
-                "Function": function.__name__,
-                "Arguments": args,
-                "Keyword arguments": kwargs,
-                "Result": result,
-                "Exception": potential_exception,
-            }
-            with open(log_path, "w") as file_writer:
-                json.dump(writing_dictionary, file_writer, default=str, indent="\t")
-
-    return wrapper
+    # Define the decorator to return.
+    def decorator(function: FunctionType) -> FunctionType:
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+        
+            # What to do before executing the function.
+            start_time = datetime.now()
+            potential_exception = None
+    
+            # Try to execute the function.
+            try:
+                result = function(*args, **kwargs)
+                return result
+            except Exception as e:
+                potential_exception = e
+    
+            # What to do after executing the function.
+            finally:
+                end_time = datetime.now()
+                writing_dictionary = {
+                    "Start time": start_time,
+                    "End time": end_time,
+                    "Working directory": Path().absolute(),
+                    "Module": function.__module__,
+                    "Function": function.__name__,
+                    "Arguments": args,
+                    "Keyword arguments": kwargs,
+                    "Result": result,
+                    "Exception": potential_exception,
+                }
+                with open(log_path, "w") as file_writer:
+                    json.dump(writing_dictionary, file_writer, default=str, indent="\t")
+    
+        return wrapper
+    return decorator
 
 
 @logged("log.json")
