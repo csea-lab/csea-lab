7ffe429187de98164195c6589ccf857a26d92349
veliebm <veliebm@gmail.com>
2021-10-07 08:34:43 -0400

    Update to be ready for new repo

--- Libas/FFT_spectrum3D_singtrial.m
+++ Libas/FFT_spectrum3D_singtrial.m
@@ -1,6 +1,6 @@
-function [specout3d, freqs] = FFT_spectrum3D_singtrial(Data3d, timewinSP, SampRate);
+function [ampout3d, phaseout3d, freqs] = FFT_spectrum3D_singtrial(Data3d, timewinSP, SampRate)
    
-fRes = 1000./(size(Data3d, 2).*(1000/SampRate));
+fRes = 1000./(size(Data3d(:,timewinSP,:) , 2).*(1000/SampRate));
 freqs = 0:fRes:SampRate./2; 
 
    for trial = 1: size(Data3d,3)
@@ -9,12 +9,14 @@ freqs = 0:fRes:SampRate./2;
         Data = Data .* cosinwin(20,size(Data,2), size(Data,1));  
 
         NFFT = size(Data,2); 
-        NumUniquePts = ceil((NFFT+1)/2); 
+        
         fftMat = fft(Data', NFFT);  % transpose: channels as columns (fft columnwise)
-        Mag = abs(fftMat);                                                   % 
+        Mag = abs(fftMat);  
+        phase = angle(fftMat); 
+        
         Mag = Mag*2;   
 
-        Mag(1) = Mag(1)/2;                                                    % DC trat aber nicht doppelt auf
+        Mag(1) = Mag(1)/2;                                                    
         if ~rem(NFFT,2),                                                    % Nyquist Frequenz (falls vorhanden) auch nicht doppelt
             Mag(length(Mag))=Mag(length(Mag))/2;
         end
@@ -22,8 +24,10 @@ freqs = 0:fRes:SampRate./2;
         Mag=Mag/NFFT; % FFT so skalieren, da? sie keine Funktion von NFFT ist
 
         Mag = Mag'; 
+        phase = phase';
 
-        specout3d(:, :, trial) = Mag(:,1:round(NFFT./2)); 
+        ampout3d(:, :, trial) = Mag(:,1:round(NFFT./2)); 
+        phaseout3d(:, :, trial) = phase(:,1:round(NFFT./2)); 
  
     end  % loop over trials
 
