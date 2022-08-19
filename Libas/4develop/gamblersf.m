4d419c17e3cfbb65a2e7b2146a4396547d790729
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2022-08-19 12:19:28 -0400

    updates

--- Libas/4develop/gamblersf.m
+++ Libas/4develop/gamblersf.m
@@ -3,6 +3,9 @@ function [w, delta] = gamblersf(params, r)
 % learn weights w based on inputs u and rewards r with learning rate eta
 %delta = prediction error for each trial
 
+%invert contingencies for gambler's fallacy
+r = (r.*-1) + 1; 
+
 eta = params(1); %learning rate
 % u= params(2); %saliency of US?
 u = 1;
@@ -12,14 +15,11 @@ dim = size(u,2); % establish number of feature dimensions
 w = zeros(trials+1, dim);
 delta = zeros(trials+1, dim);%prediction error
 
-
 for t=2:trials
-    delta(t) = u .* eta .* (-1.*(w(t))-r(t));
-    w(t+1) = w(t) + delta(t);
-    if w(t) > 1 
-       w(t) = 1;
-    end
+    delta(t) = u .* eta .* ((w(t))-r(t));
+    w(t+1) = ((w(t) - delta(t)));   
 end
+
 w(isinf(w)) = 1;
 w = w(2:end); 
 
--- Libas/4develop/gamblersf_ak.m
+++ Libas/4develop/gamblersf_ak.m
@@ -3,20 +3,22 @@ function [w, delta] = gamblersf_ak(params, r)
 % learn weights w based on inputs u and rewards r with learning rate eta
 %delta = prediction error for each trial
 
+%invert contingencies for gambler's fallacy
+r = (r.*-1) + 1; 
+
 eta = params(1); %learning rate
 % u= params(2); %saliency of US?
 u = 1;
 
 trials = size(r,1); % establish number of trials
 dim = size(u,2); % establish number of feature dimensions
-w = zeros(trials+1, dim) + .5;
+w = zeros(trials+1, dim);
 delta = zeros(trials+1, dim);%prediction error
 
 for t=2:trials
     delta(t) = u .* eta .* ((w(t))-r(t));
     w(t+1) = ((w(t) - delta(t)));   
 end
-
 
 w(isinf(w)) = 1;
 w = w(2:end); 
