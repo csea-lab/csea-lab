f8640533fdbb49b3860deb5540b6ff8e4e941ad3
akeilCSEA <akeil@ufl.edu>
2022-11-01 20:15:03 -0400

    Update shreadpostpro.m

--- Libas/4scriptsandpipelines/shreadpostpro.m
+++ Libas/4scriptsandpipelines/shreadpostpro.m
@@ -2,8 +2,15 @@ function [PLImat] = shreadpostpro(datfilepath, setfilepath_p, setfilepath_i)
 
 eeglab
 
+<<<<<<< Updated upstream
 PLImat = [];
 
+=======
+
+PLImat = [];
+
+
+>>>>>>> Stashed changes
 [convec] = SHREAD_getcontrial(datfilepath); 
 
 EEG_p = pop_loadset(setfilepath_p);
@@ -12,5 +19,8 @@ EEG_i = pop_loadset(setfilepath_i);
 
 
 
+<<<<<<< Updated upstream
 
 
+=======
+>>>>>>> Stashed changes
