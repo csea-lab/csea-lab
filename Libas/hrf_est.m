c4f4e5429caf6237a43528ea9a04aefb2e9c77b5
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2023-02-09 12:00:49 -0500

    Update hrf_est.m

--- Libas/hrf_est.m
+++ Libas/hrf_est.m
@@ -1,10 +1,10 @@
 function [h] = hrf_est(BOLDvec, scanonsets, hrflength)
-% bold vector needs to be row vector, scanonsets is stim onset times as
+% bold vector needs to be row vector, scanonsets is stim onset times, as
 % indices into BOLD vector 
-%hrflength is a scalar (a number) indicating how many scans should be
-%modeled 
+% hrflength is a scalar (a number) indicating how many scans should be
+% modeled (length of the kernel)
 
-%first, make the "design matrix"
+%first, make the design matrix
 s = zeros(1,length(BOLDvec))';
 s(scanonsets) = 1;
 
