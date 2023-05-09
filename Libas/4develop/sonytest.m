a41c9451b4ea5dc1c6f8b635284371201208c186
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2023-05-09 10:42:09 -0400

    Update sonytest.m

--- Libas/4develop/sonytest.m
+++ Libas/4develop/sonytest.m
@@ -7,7 +7,7 @@ time = 0.001:0.001:5; % Five seconds of discrete time, sampled at 1000 Hz
 
 for x = 1:20 
 
-    for trial = 1:30
+    for trial = 1:60
 
         temp1 = rand(size(time))-.5; % zero-centered white nois
         brownsig = cumsum(temp1);  % Brownian noise is the cumulative sum of white noise
@@ -15,6 +15,7 @@ for x = 1:20
         testsig2(trial,:)  = detrend(brownsig-mean(brownsig)); % zero-centered Brownian noise
         testsig2(trial, 3001:4000) =  testsig2(3001:4000) + SinWave; % add the sine wave
        
+        SNR(trial, x) = x./range(testsig2(trial, :));
 
     end
 
@@ -27,10 +28,15 @@ end
 
 
 %%
-for x = 1:30,
+for x = 1:60,
     for y = 1:20,
         if ~isempty(MPP(x,y).Trials)
             aha(x,y) = MPP(x,y).Trials(1).tau; 
         end
     end
+end
+
+%%
+for x = 1:20
+    outvec(x) = length(find(abs(aha(:, x)-3500) < 400));
 end
