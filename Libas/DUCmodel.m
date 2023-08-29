bd436245191843a7f3e1ed5a247b10a44c24aa5f
akeilCSEA <akeil@ufl.edu>
2023-08-29 17:35:28 -0400

    new modeling functions

--- Libas/DUCmodel.m
+++ Libas/DUCmodel.m
@@ -13,14 +13,8 @@ baseline2 = zeros(1,6000);
 a = [baseline1  params(1) .* cosinwin(ramp, length(taxis))];
 b = [baseline2  params(2) .* cosinwin(ramp, length(taxis))];
 
-
 earlyinterference = [baseline1 cosinwin(ramp, ramp.*3) zeros(1,length(taxis)-ramp.*3)].*params(3); 
 lateinterference = [baseline1 zeros(1, ramp.*3) cosinwin(ramp,length(taxis)-ramp.*3)].*params(5); 
-
-
-%plot(earlyinterference), pause
-%plot(lateinterference), pause
-
 
 ssvep_distractor = a + earlyinterference + lateinterference;  
 ssvep_task = b + earlyinterference.*params(4) - lateinterference; 
