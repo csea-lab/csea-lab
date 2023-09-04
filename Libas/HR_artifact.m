8cb0f96e9d0ae87fa41c181f546515c6bba77129
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2023-09-04 12:50:39 -0400

    HR code updated

--- Libas/HR_artifact.m
+++ Libas/HR_artifact.m
@@ -24,7 +24,7 @@ IBIvecClean = IBIvecClean1;
 count1 = 1; 
 count2 = 1;
 
-for index = 1:length(IBIvecClean1)
+for index = 1:length(IBIvecClean1)-1
    if IBIvecClean1(index) < .5
        IBIvecClean(count1) = IBIvecClean1(index) + IBIvecClean1(index+1); 
        IBIvecClean(count1+1) = nan;
