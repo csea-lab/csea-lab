a4c346201c1dbdc02e026e0d17f65cc9e1a857bc
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2023-10-25 15:42:52 -0400

    Update HR_script_wurz.m

--- Libas/4scriptsandpipelines/HR_script_wurz.m
+++ Libas/4scriptsandpipelines/HR_script_wurz.m
@@ -45,3 +45,31 @@ plot(taxis, mean(groupBPM3bsl), 'g', 'LineWidth', 4)
 plot(taxis,mean(groupBPM2bsl), 'm', 'LineWidth', 4)
 plot(taxis,mean(groupBPM1bsl), 'r', 'LineWidth', 4)  
 
+%% for sending to wuerzburg
+cd '/Users/andreaskeil/Desktop/app2HR'
+
+clear
+
+filemat = getfilesindir(pwd, '*HR.mat'); 
+
+for fileindex = 1:size(filemat,1) 
+
+    temp = load(deblank(filemat(fileindex,:))); 
+
+    trialBPM = movmean(temp.BPMmat(:, 2:end), 5, 2);
+
+    stdvec = std(trialBPM');
+
+    badindex = find(stdvec>4);  
+    
+    avgBPM = mean(trialBPM); 
+
+    trialBPM(badindex, :) = [];
+
+    trialBPM(end+1, :) = ones(size(trialBPM(1,:))).* 60; 
+    
+
+   % plot(trialBPM'), title(deblank(filemat(fileindex,:))), hold on, plot(avgBPM, 'k', 'LineWidth', 4), pause, hold off
+
+end
+   disp('done')
