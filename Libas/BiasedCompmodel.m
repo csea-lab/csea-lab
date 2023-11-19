717277023951ab3689d90e301a0b64543c5b6bc2
akeilCSEA <akeil@ufl.edu>
2023-11-19 15:26:13 -0500

    Update BiasedCompmodel.m

--- Libas/BiasedCompmodel.m
+++ Libas/BiasedCompmodel.m
@@ -8,15 +8,17 @@ baseline2 = zeros(1,6000);
 % params(2) = level2, aka drive 2
 % params(3) = early response
 % params(4) = gain
-% params(5) = bias
+% params(5) = bias/scaling factor
 
-a = [baseline1  cosinwin(ramp, length(taxis))];
+a = [baseline1 cosinwin(ramp, length(taxis))];
 b = [baseline2  params(2) .* cosinwin(ramp, length(taxis))];
 
 earlyresponse = [baseline1 cosinwin(ramp, ramp.*3) zeros(1,length(taxis)-ramp.*3)].*params(3); 
 
-normalization1 = (a + earlyresponse);
+normalization1 = a + earlyresponse;
 normalization2 = b + earlyresponse;
+
+pool = (normalization1 + normalization2)./params(5);
 
 % figure(2)
 % plot(normalization1)
@@ -24,8 +26,8 @@ normalization2 = b + earlyresponse;
 % plot(normalization2)
 
 
-ssvep_distractor = params(1) .* normalization1 ./ (params(4).*(normalization1 + normalization2));
-ssvep_task = params(2) .* normalization2 ./ (params(4).*(normalization1 + normalization2));
+ssvep_distractor = params(1) .* normalization1 ./ (params(4).*pool);
+ssvep_task = params(2) .* normalization2 ./ (params(4).*pool);
 
 ssveptotal = [ssvep_distractor ssvep_task]; 
 
