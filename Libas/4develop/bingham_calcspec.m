9119e47a82cf0c5137c0e97305960adee2550282
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2023-12-14 11:13:31 -0500

    bingham suite updated

---
+++ Libas/4develop/bingham_calcspec.m
@@ -0,0 +1,29 @@
+function [spectrum] = bingham_calcspec(filemat)
+% this function takes ERP processed files ending in ERPs.mat
+% and computes a spectrum of the post-stimulus period, in the same way as
+% the postpro function does.
+% copy all the ERPs.mat files into new folder/have open as part of
+% current folder in MATLAB
+% get the filemat input by running: filemat = getfilesindir(pwd, '*ERPs.mat')
+% need emegs2.8 for 'getfilesindir' commmand
+
+spectrum = []; 
+
+for fileindex = 1:size(filemat,1)
+    
+  load(deblank(filemat(fileindex,:))); 
+  
+  [spectrum.amphappy, phase, spectrum.freqs, fftcomp] = freqtag_FFT(ERP_happy(:, 601:5600), 1000); 
+  [spectrum.ampangry, phase, freqs, fftcomp] = freqtag_FFT(ERP_angry(:, 601:5600), 1000); 
+  [spectrum.ampsad, phase, freqs, fftcomp] = freqtag_FFT(ERP_sad(:, 601:5600), 1000); 
+  
+
+  save([filemat(fileindex,1:end-9) '.Spec.mat'], 'spectrum')
+  
+       
+end
+
+
+
+end
+
