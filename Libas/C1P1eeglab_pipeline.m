acd5640592dc6b513c64c4f410811807c79566fd
katemccain <mccain.k@ufl.edu>
2024-01-15 11:43:07 -0500

    Added event codes to C1P1eeglab_pipeline
    Added event codes to C1P1eeglab_pipeline

--- Libas/C1P1eeglab_pipeline.m
+++ Libas/C1P1eeglab_pipeline.m
@@ -5,10 +5,12 @@ EEG = pop_readegi(EEGfilepath, [],[],'auto');
 
 EEG = eeg_checkset( EEG );
 
+codes = load("event_codes_c1p1_test.txt");
+
 % filter the data
 temp = EEG.data'; 
 
-[fila,filb] = butter(4, 0.08); 
+[fila,filb] = butter(4, 0.06); 
 
 temp2 = filtfilt(fila, filb, double(temp)); 
 
@@ -25,6 +27,11 @@ EEG=pop_chanedit(EEG, 'load',{'GSN-HydroCel-257.sfp' 'filetype' 'autodetect'},'c
 
 EEG = eeg_checkset( EEG );
 
+EEG = pop_eegchanoperator( EEG, '/Volumes/My Passport/c1p1/equations_list.txt');
+
+EEG=pop_chanedit(EEG, 'load',{'GSN-HydroCel-257.sfp' 'filetype' 'autodetect'});
+
+
 %% now the trigger issue, har har 
 for index1 = 1:length(EEG.event)
 temptrigs(index1) = EEG.event(index1).latency;
@@ -39,32 +46,45 @@ newtrigvec = [temptrigs(1); newtrigvec_temp'];
 EEG.event = []; 
 EEG.urevent = []; 
 
+% for index2 = 1:length(newtrigvec)
+%     EEG.event(index2).type = num2str(codes(index2)); 
+%     EEG.event(index2).latency = newtrigvec(index2);
+%     EEG.event(index2).urevent = index2;
+%     EEG.urevent(index2).type = 'DIN3';
+%     EEG.urevent(index2).latency = newtrigvec(index2);
+% end
+
+event_codes = arrayfun(@num2str, codes, 'UniformOutput', false);
+
 for index2 = 1:length(newtrigvec)
-    EEG.event(index2).type = 'DIN3';
+    EEG.event(index2).type = event_codes{index2};
     EEG.event(index2).latency = newtrigvec(index2);
     EEG.event(index2).urevent = index2;
-    EEG.urevent(index2).type = 'DIN3';
+    EEG.urevent(index2).type = event_codes{index2};
     EEG.urevent(index2).latency = newtrigvec(index2);
 end
 
 
 
-EEG = pop_epoch( EEG, {  'DIN3'  }, [-.2  .3], 'newname', 'test epochs', 'epochinfo', 'yes');
+%resample to 250 Hz
+EEG = pop_resample( EEG, 250);
 EEG = eeg_checkset( EEG );
-EEG = pop_rmbase( EEG, [-100     0]);
+
+%run the ICA and save  output
+EEG = pop_runica(EEG,  'icatype', 'sobi');
+EEG = eeg_checkset( EEG );
+% EEG = pop_saveset( EEG, 'filename', '515.EEG.set','filepath',pwd);
+% EEG = eeg_checkset( EEG );
 
 
-% take care of bad channels
- [outmat3d, interpsensvec] = scadsAK_3dchan(EEG.data, EEG.chanlocs);
- EEG.data = single(outmat3d); 
-
+% EEG = pop_epoch( EEG, {  'DIN3'  }, [-.2  .3], 'newname', 'test epochs', 'epochinfo', 'yes');
+% EEG = eeg_checkset( EEG );
+% EEG = pop_rmbase( EEG, [-100     0]);
 % 
-% % run the ICA and save  output
-%  EEG = pop_runica(EEG,  'icatype', 'sobi');
-%  EEG = eeg_checkset( EEG );
-%  EEG = pop_saveset( EEG, 'filename',[EEGfilepath '.EEG.set'],'filepath',pwd);
-%  EEG = eeg_checkset( EEG );
-%  
+% % take care of bad channels
+% [outmat3d, interpsensvec] = scadsAK_3dchan(EEG.data, EEG.chanlocs);
+% EEG.data = single(outmat3d); 
+
 % % 
 %  warning('off');
 %  pop_topoplot(EEG,0, [1:64] ,'component topographies',[8 8] ,0,'electrodes','off');
