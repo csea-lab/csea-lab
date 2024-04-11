a891c54da0ecb24063e8a2d358e62e4a59606d65
A-Mirifar <128939712+A-Mirifar@users.noreply.github.com>
2024-04-11 17:10:51 -0400

    Update WurzEEGlab2WaPow.m

--- Libas/4develop/WurzEEGlab2WaPow.m
+++ Libas/4develop/WurzEEGlab2WaPow.m
@@ -5,9 +5,9 @@ for index = 1:size(filemat, 1)
    temp = load(filemat(index,:));
 
    % now do three wavelet analyses
-    wavelet_app_mat(double(temp.EEG_CerThreat.data), 500, 50, 300, 10, 72, [filemat(index,1:10) '.CerThreat']);
-    wavelet_app_mat(double(temp.EEG_PoThreat.data), 500, 50, 300, 10, 72, [filemat(index,1:10) '.PoThreat']);
-    wavelet_app_mat(double(temp.EEG_Safe.data), 500, 50, 300, 10, 72, [filemat(index,1:10) '.Safe']);
+    wavelet_app_mat(double(temp.EEG_CerThreat.data), 500, 50, 300, 10, 72, [filemat(index,1:15) '.CerThreat']);
+    wavelet_app_mat(double(temp.EEG_PoThreat.data), 500, 50, 300, 10, 72, [filemat(index,1:15) '.PoThreat']);
+    wavelet_app_mat(double(temp.EEG_Safe.data), 500, 50, 300, 10, 72, [filemat(index,1:15) '.Safe']);
 
 end
 
@@ -28,15 +28,15 @@ filemat1 = filemat(1:3:end,:)
 filemat2 = filemat(2:3:end,:)
 filemat3 = filemat(3:3:end,:)
 
-avgmats_mat(filemat3, 'GM49.pow3.app3.mat');
-avgmats_mat(filemat2, 'GM49.pow3.app2.mat');
-avgmats_mat(filemat1, 'GM49.pow3.app1.mat');
+avgmats_mat(filemat3, 'GM50.pow3.app3.mat');
+avgmats_mat(filemat2, 'GM50.pow3.app2.mat');
+avgmats_mat(filemat1, 'GM50.pow3.app1.mat');
 
-load('GM49.pow3.app1.mat')
+load('GM50.pow3.app1.mat')
 avgmat1 = avgmat;
-load('GM49.pow3.app2.mat')
+load('GM50.pow3.app2.mat')
 avgmat2 = avgmat;
-load('GM49.pow3.app3.mat')
+load('GM50.pow3.app3.mat')
 avgmat3 = avgmat;
 
 
@@ -46,7 +46,7 @@ avgmatbsl2 = bslcorrWAMat_percent(avgmat2, 50:200);
 avgmatbsl3 = bslcorrWAMat_percent(avgmat3, 50:200);
 
 figure
-elec = 87
+elec = 72
 subplot(3,1,1), contourf(taxis(50:end-50), faxis(2:end), squeeze(avgmatbsl1(elec,50:end-50, 2:end))', 15), caxis([-30 10]), colorbar
 subplot(3,1,2), contourf(taxis(50:end-50), faxis(2:end), squeeze(avgmatbsl2(elec,50:end-50, 2:end))', 15), caxis([-30 10]), colorbar
 subplot(3,1,3), contourf(taxis(50:end-50), faxis(2:end), squeeze(avgmatbsl3(elec,50:end-50, 2:end))', 15), caxis([-30 10]), colorbar
