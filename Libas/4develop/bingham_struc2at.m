66427f9da1f23960cf600c7f825d9f7124829b2d
hannahengle <hengle@ufl.edu>
2024-05-23 15:48:52 -0400

    Update bingham_struc2at.m

--- Libas/4develop/bingham_struc2at.m
+++ Libas/4develop/bingham_struc2at.m
@@ -1,5 +1,6 @@
 function bingham_struc2at(filepath)
-% navigate into folderthat has output from bingham_avgsubj
+% navigate into folder that has output from bingham_avgsubj, filed suffix
+% should be something along the lines of GMspectrum.mat
 % and then bingham_struc2at(filepath), where filepath is the .mat file
 
 temp1 = load(filepath); 
@@ -7,5 +8,13 @@ temp1 = load(filepath);
 happy = temp1.GMspectrum.amphappy(1:31,:); 
 
 SaveAvgFile([filepath '.happy.at'], happy, [], [], 5000);
+
+angry = temp1.GMspectrum.ampangry(1:31,:); 
+
+SaveAvgFile([filepath '.angry.at'], happy, [], [], 5000);
+
+sad = temp1.GMspectrum.ampsad(1:31,:); 
+
+SaveAvgFile([filepath '.sad.at'], happy, [], [], 5000);
 
 end
