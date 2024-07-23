6e226861a4ae2e0a3b706dd26c30fab8440c40fa
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2024-07-23 15:02:18 -0400

    Update Ventura_prepro.m

--- Libas/4develop/Ventura_prepro.m
+++ Libas/4develop/Ventura_prepro.m
@@ -1,6 +1,7 @@
-function [specavg, freqs, mat_end, demodmat] = Ventura_prepro(filemat, plotflag)
+function [specavg, freqs2, mat_end] = Ventura_prepro(filemat, plotflag)
 
 % sample rate is 250 Hz
+fsamp = 250; 
 % mff files
 
 for fileindex = 1:size(filemat,1)
@@ -30,8 +31,12 @@ for fileindex = 1:size(filemat,1)
     load('locsEEGLAB129HCL.mat');
     locations = locsEEGLAB129HCL;
 
+     [~,S2] = regress_eog(data', 1:129, sparse([125,128,25,127,8,126],[1,1,2,2,3,3],[1,-1,1,-1,1,-1]));
+
+     data = S2'; 
+
     % interpolate bad channels throughoutlocs
-    [data, interpvecthroughout] = scadsAK_2dInterpChan(data, locations, 3);
+    [data, interpvecthroughout] = scadsAK_2dInterpChan(data, locations, 2);
 
     % this is the first step after reading stuff in: 
     % find the times (in sample points) where a stm+ event happened
@@ -48,30 +53,34 @@ for fileindex = 1:size(filemat,1)
 
 
     % now find the data segments and get the ERP data
-    mat = zeros(129, 1201, size(segmentvec,2));
+    mat = zeros(128, 1201, size(segmentvec,2));
     for x = 1:size(segmentvec,2)
-    mat(:, :, x) = data(:, segmentvec(x)-100:segmentvec(x)+1100);  
+    mat(:, :, x) = data(1:128, segmentvec(x)-100:segmentvec(x)+1100);  
     end
 
     % do artifact correction
     % first bad channels within trials
-    [ mat_end, badindex, NGoodtrials ] = scadsAK_3dtrials(mat, 1.5); 
+    [ mat_end, badindex, NGoodtrials ] = scadsAK_3dtrials(mat, 1); 
 
     % average
     mat_end = avg_ref_add3d(mat_end); 
 
-    [specavg, freqs] = FFT_spectrum3D(mat_end, 600:1100, 250);
-    [demodmat, phasemat, complexmat]=steadyHilbertMat(mean(mat_end,3), 7.5, 1:100, 10, 1, 250);
+    ERP = squeeze(mean(mat_end,3));
+
+    [specavg, freqs1] = FFT_spectrum3D(mat_end, 101:1100, 250);
+    [spec, ~, freqs2] = FFT_spectrum(ERP(:, 101:1100), 250);
+    %[demodmat, phasemat, complexmat]=steadyHilbertMat(mean(mat_end,3), 5, 1:100, 10, 1, 250);
 
     if plotflag
-    figure
-    plot(freqs(1:80), specavg(:, 1:80)')
+    figure(300)
+    subplot(2,1,1), plot(freqs1(1:80), specavg(:, 1:80)')
+    subplot (2,1,2), plot(freqs2(1:80), spec(:, 1:80)')
     pause(1)
     end
 
-    eval(['save ' filepath(1:15) '.spec.mat specavg -mat'])
-    
-    fprintf('.')
+    fakesamplerate = 1000./(fsamp./(size(spec,2).*2));
+    SaveAvgFile([filemat(fileindex,1:end-3) 'at.spec'],spec,[],[],fakesamplerate); 
+
     
 end
 
