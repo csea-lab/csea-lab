b2d71f92cf4a2ba837ca9d2e43659eb42c3b364f
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2024-09-03 12:09:37 -0400

    Update TimeDomMorletWavelet.m

--- Libas/4develop/TimeDomMorletWavelet.m
+++ Libas/4develop/TimeDomMorletWavelet.m
@@ -1,7 +1,9 @@
-function [outvec] = TimeDomMorletWavelet(f0, sigma, t)
+function [amplitude] = TimeDomMorletWavelet(beta, t)
 
+f0 = beta(1);
+sigma = beta(2);
 
-amplitude = (pi^(-0.25)) * exp(2i * pi * f0 * t) .* exp(-t.^2 / (2 * sigma^2));
+temp = (pi^(-0.25)) * exp(2i * pi * f0 * t) .* exp(-t.^2 / (2 * sigma^2));
 
-outvec = real(amplitude); 
+amplitude = real(temp); 
 
