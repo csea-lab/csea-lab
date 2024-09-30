44e5ce6bff1d6a2ab9d2c7f2bde006edc0b89682
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2024-09-30 12:37:58 -0400

    Update BiasedCompmodel.m

--- Libas/BiasedCompmodel.m
+++ Libas/BiasedCompmodel.m
@@ -1,8 +1,12 @@
 function [ssveptotal] = BiasedCompmodel(params,taxis)
 
-ramp = 600;
-baseline1 = zeros(1,6000);
-baseline2 = zeros(1,6000); 
+
+
+baselinelength = 500; 
+signallength = length(taxis)-baselinelength; 
+ramp = 100; % a meta parameter for the duration of the onset
+baseline1 = zeros(1,baselinelength); % duration of distractor bsl 
+baseline2 = zeros(1,baselinelength); % duration of task bsl 
 
 % params(1) = level1, aka drive 1
 % params(2) = level2, aka drive 2
@@ -10,10 +14,10 @@ baseline2 = zeros(1,6000);
 % params(4) = gain
 % params(5) = bias/scaling factor
 
-a = [baseline1  params(1) .*cosinwin(ramp, length(taxis))];
-b = [baseline2  params(2) .* cosinwin(ramp, length(taxis))];
+a = [baseline1  params(1) .*cosinwin(ramp, signallength)];
+b = [baseline2  params(2) .* cosinwin(ramp, signallength)];
 
-earlyresponse = [baseline1 cosinwin(ramp, ramp.*3) zeros(1,length(taxis)-ramp.*3)].*params(3); 
+earlyresponse = [baseline1 cosinwin(ramp, ramp.*3) zeros(1,signallength-ramp.*3)].*params(3); 
 
 normalization1 = (a + earlyresponse.*params(4) ).* params(5);
 normalization2 = (b + earlyresponse).* -params(5);
