4486ef7e80eca4893da61ce6dc0ae32dbb49b0fe
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2024-11-15 08:38:03 -0500

    Update make_alpha_simuldata.m

--- Libas/alpha_simulation/make_alpha_simuldata.m
+++ Libas/alpha_simulation/make_alpha_simuldata.m
@@ -1,4 +1,7 @@
 cd '/Users/andreaskeil/Documents/GitHub/csea-lab/Libas/alpha_simulation'
+nsubjects = 30;
+
+effectsize = .1;
 
 a = read_avr('alphafake.avr'); % read alpha topography, saved from Besa sim
 
@@ -10,17 +13,40 @@ c = resample(b, 5, 1, 'Dimension', 2); % alpha topography with alpha time course
 alpha_template = fliplr(c); 
 % alpha sig starts at sample point 1, ends at 800
 
-datatemp = zeros(129, 2000, 40); 
+mat4D_1 = zeros(129, 2000, 28, nsubjects); 
+mat4D_2 = zeros(129, 2000, 28, nsubjects);
 
-for trial = 1:40
+data1 = zeros(129, 2000, 40); 
+data2 = zeros(129, 2000, 40); 
 
-    whitenoise = rand(129,2000); 
-    tempbrown = cumsum(whitenoise'-.5);
-    brownnoise = detrend(tempbrown)';
-    data_trial = brownnoise; 
-    data_trial(:, 1:1000) = data_trial(:, 1:1000)+alpha_template; 
-    datatemp(:, :, trial) = data_trial;
+for subject = 1:nsubjects
+
+    for trial = 1:40
+    % data1 has alpha template
+        whitenoise = rand(129,2000);
+        tempbrown = cumsum(whitenoise'-.5);
+        brownnoise = detrend(tempbrown)';
+        data_trial = brownnoise;
+        data_trial(:, 1:1000) = data_trial(:, 1:1000)+alpha_template.*effectsize;
+        data1(:, :, trial) = data_trial;
+        
+     %data2 has no alpha template 
+        whitenoise = rand(129,2000);
+        tempbrown = cumsum(whitenoise'-.5);
+        brownnoise = detrend(tempbrown)';
+        data2(:, :, trial) = brownnoise;
+
+    end
+
+    [WaPower1] = wavelet_app_mat(data1, 1000, 5, 60, 2, []);
+    [WaPower2] = wavelet_app_mat(data2, 1000, 5, 60, 2, []);
+
+    [outWaMat1] = bslcorrWAMat_percent(WaPower1, 300:600);
+    [outWaMat2] = bslcorrWAMat_percent(WaPower2, 300:600);
+
+    mat4D_1(:, :, :, subject) = outWaMat1;
+    mat4D_2(:, :, :, subject) = outWaMat2;
 
 end
 
-[WaPower, PLI, PLIdiff] = wavelet_app_mat(datatemp, 1000, 5, 60, 2, [], []);
+[~, ~, ~, stats] = ttest(mat4D_1,mat4D_2,"Dim",4);
