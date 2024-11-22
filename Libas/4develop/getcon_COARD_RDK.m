c55ccc5c9611afc0402ff52833564cb3bbbb5c28
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2024-11-22 10:58:24 -0500

    Update getcon_COARD_RDK.m

--- Libas/4develop/getcon_COARD_RDK.m
+++ Libas/4develop/getcon_COARD_RDK.m
@@ -1,10 +1,16 @@
-function [ conditionvector, targetpresent, reportvector] = getcon_COARD_RDK(datfilepath)
+function [ conditionvector, percentcorrect, targetpresent, reportvector] = getcon_COARD_RDK(datfilepath)
 
-conditionvector = []; 
+conditionvector = [];
 
-targetpresent = []; 
+targetpresent = [];
 
-reportvector = []; 
+reportvector = [];
+
+percentcorrect = zeros(1,4);
+
+targvec = []; 
+
+repvec =[];
 
 fid = fopen(datfilepath);
 
@@ -14,29 +20,36 @@ a = 1;
 
 
 while a > 0
-    
+
     a = fgetl(fid);
-        
+
     if a < 0, break, return, end
-    
+
     blankindex = find(a == ' ');
 
-   letter =  a(blankindex(7)+1);
+    letter =  a(blankindex(7)+1);
 
-   targetpresent =  [targetpresent; str2num(a(blankindex(3)+1))];
-   reportvector = [reportvector; str2num(a(blankindex(5)+1))];
+    targetpresent =  [targetpresent; str2num(a(blankindex(3)+1))];
+    reportvector = [reportvector; str2num(a(blankindex(5)+1))];
 
-   if strcmp(letter, 'p'), conditionvector  = [conditionvector 1]; 
-   elseif strcmp(letter, 'n'), conditionvector  = [conditionvector 2]; 
-   elseif strcmp(letter, 'u'), conditionvector  = [conditionvector 3];
-   else conditionvector  = [conditionvector 4];
-   end
+    if strcmp(letter, 'p'), conditionvector  = [conditionvector 1];
+    elseif strcmp(letter, 'n'), conditionvector  = [conditionvector 2];
+    elseif strcmp(letter, 'u'), conditionvector  = [conditionvector 3];
+    else conditionvector  = [conditionvector 4];
+    end
 
 end
 
 
 conditionvector = column(conditionvector);
 
+crosstab(targetpresent, reportvector)
+
+percentcorrectoverall = 1-sum(diag( crosstab(targetpresent, reportvector)))./sum(sum(crosstab(targetpresent, reportvector)))
 
 
-
+for condition=1:4
+    targvec =  targetpresent(conditionvector==condition);
+    repvec = reportvector(conditionvector==condition);
+    percentcorrect(condition) =1-sum(diag( crosstab(targvec, repvec)))./sum(sum(crosstab(targvec, repvec)));
+end
