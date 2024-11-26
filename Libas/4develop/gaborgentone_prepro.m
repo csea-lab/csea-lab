42108617909507f15565655f0f5c8cf83dc2b66f
SarahMarieG <135059341+SarahMarieG@users.noreply.github.com>
2024-11-26 14:10:19 -0500

    created and ran code for gaborgentone preprocessing

---
+++ Libas/4develop/gaborgentone_prepro.m
@@ -0,0 +1,39 @@
+%% Script for analyzing gaborgentone data
+% Open eeglab and get a list of all files and folders in the current directory
+temp99 = eeglab; 
+
+files = dir("gg*");
+
+% Filter out the non-folder entries
+dirFlags = [files.isdir];
+
+% Extract the names of the folders
+folderNames = {files(dirFlags).name};
+
+% Remove the '.' and '..' folders
+folderNames = folderNames(~ismember(folderNames, {'.', '..'}));
+
+% Display the folder names
+disp('Folders in the current working directory:');
+disp(folderNames);
+
+% loop over subjects
+for subindex = 1:size(folderNames,2)
+
+    eval(['cd ' folderNames{subindex}])
+
+   delete *.at*
+
+    datfile = getfilesindir(pwd, '*.dat');
+    rawfile = getfilesindir(pwd, '*.RAW');
+
+    % actual preprocessing
+   prepro_scadsandspline_log(rawfile, datfile, 'getcon_gaborgenTone', 16, {'21' '22' '23' '24'}, [-.6 3], [1  30], 4, 1)
+   % prepro_scadsandspline(datapath, logpath, convecfun, stringlength, conditions2select, timevec, filtercoeffHz, filtord, skiptrials)
+
+    cd ..
+
+    pause(.5)
+    fclose('all');
+
+end
