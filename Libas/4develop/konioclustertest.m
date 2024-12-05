1cb0e6c8c1faf9b3c36fa4c8b1b625474fcf63f5
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2024-12-05 15:40:13 -0500

    Create konioclustertest.m

---
+++ Libas/4develop/konioclustertest.m
@@ -0,0 +1,41 @@
+
+cd '/Users/andreaskeil/Desktop/Konioproject 2024/hampfiles'
+
+filemat = getfilesindir(pwd, '*hamp8');
+
+filemat21 = filemat(1:4:end,:);
+filemat22 = filemat(2:4:end,:);
+
+[topo_tmat, mat3d_1, mat3d_2] = topottest(filemat22, filemat21, [], 'ttest22vs21');
+
+datmat4d = cat(4,mat3d_1, mat3d_2);
+
+for loop = 1:1000
+
+     datmat4d_perm = datmat4d; 
+
+    for sub= 1:50
+
+        datmat4d_perm(:, :, sub,:) =  datmat4d(:, :, sub,randperm(2));
+
+    end
+
+        [~, ~, ~, stats] = ttest(squeeze(datmat4d_perm(:, :, :, 2)), squeeze(datmat4d_perm(:, :, :, 1)), 'Dim',3);
+
+        [cluster_out_pos_perm, cluster_out_neg_perm] = LB3_findclusters_cbp_3D(stats.tstat, cube_coords, 2.06, 1, 0, 0);
+
+
+        distribMAX(loop) = max(cluster_out_pos_perm.sum); 
+        distribMIN(loop) = min(cluster_out_neg_perm.sum); 
+
+        if round(loop./100) == loop/100, fprintf('.'), end
+
+end
+
+
+
+
+
+
+
+
