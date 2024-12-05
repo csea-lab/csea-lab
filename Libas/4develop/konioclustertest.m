38521b3cd8e53a28a18d66c2391f54e29a4bb6b0
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2024-12-05 16:22:21 -0500

    Update konioclustertest.m

--- Libas/4develop/konioclustertest.m
+++ Libas/4develop/konioclustertest.m
@@ -3,6 +3,9 @@ cd '/Users/andreaskeil/Desktop/Konioproject 2024/hampfiles'
 
 filemat = getfilesindir(pwd, '*hamp8');
 
+distribMAX = []; 
+distribMIN = []; 
+
 filemat21 = filemat(1:4:end,:);
 filemat22 = filemat(2:4:end,:);
 
@@ -22,11 +25,15 @@ for loop = 1:1000
 
         [~, ~, ~, stats] = ttest(squeeze(datmat4d_perm(:, :, :, 2)), squeeze(datmat4d_perm(:, :, :, 1)), 'Dim',3);
 
-        [cluster_out_pos_perm, cluster_out_neg_perm] = LB3_findclusters_cbp_3D(stats.tstat, cube_coords, 2.06, 1, 0, 0);
+        [cluster_out_pos_perm, cluster_out_neg_perm] = LB3_findclusters_cbp_3D(stats.tstat, cube_coords, 2.79, 1, 0, 0);
 
-
+        if ~isempty(cluster_out_pos_perm.sum)
         distribMAX(loop) = max(cluster_out_pos_perm.sum); 
+        end
+        
+        if ~isempty(cluster_out_neg_perm.sum)
         distribMIN(loop) = min(cluster_out_neg_perm.sum); 
+        end
 
         if round(loop./100) == loop/100, fprintf('.'), end
 
