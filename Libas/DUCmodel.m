623b3a310dfea684a14195fd7e8481eb9719dfc9
akeilCSEA <akeil@ufl.edu>
2025-01-05 13:08:57 -0500

    Update DUCmodel.m

--- Libas/DUCmodel.m
+++ Libas/DUCmodel.m
@@ -15,8 +15,12 @@ baseline2 = zeros(1,baselinelength); % duration of task bsl
 a = [baseline1  params(1) .* cosinwin(ramp, signallength)];
 b = [baseline2  params(2) .* cosinwin(ramp, signallength)];
 
-earlyinterference = [baseline1 cosinwin(ramp, ramp.*3) zeros(1,signallength-ramp.*3)].*params(3); 
+ earlyinterference = [baseline1 cosinwin(ramp, ramp.*3) zeros(1,signallength-ramp.*3)].*params(3); 
 lateinterference = [baseline1 zeros(1, ramp.*3) cosinwin(ramp,signallength-ramp.*3)].*params(5); 
+
+% adding a sixth parameter for latency variability makes fits worse
+%earlyinterference = [baseline1 cosinwin(ramp, params(6)) zeros(1,signallength-round(params(6))-ramp)].*params(3); 
+% lateinterference = [baseline1 zeros(1, round(params(6))) cosinwin(ramp,signallength-params(6))].*params(5); 
 
 ssvep_distractor = a + earlyinterference + lateinterference;  
 ssvep_task = b + earlyinterference.*params(4) - lateinterference; 
