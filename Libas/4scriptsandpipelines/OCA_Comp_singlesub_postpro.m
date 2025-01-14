6a8bea03a1c5e10d3e8bacb0a6491e3852b65fec
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2025-01-14 12:16:37 -0500

    Update OCA_Comp_singlesub_postpro.m

--- Libas/4scriptsandpipelines/OCA_Comp_singlesub_postpro.m
+++ Libas/4scriptsandpipelines/OCA_Comp_singlesub_postpro.m
@@ -2,9 +2,11 @@
 cd '/Volumes/G-RAID Thunderbolt 3/OCA_project/OCA_comp'
 
 % info for the analysis
- spectime = 1001:4000;
+% tag frequencies are at 6.666 and 15 Hz
+ spectime = 1051:4050;
  Fbin1 = 21; 
  Fbin2 = 46; 
+ taxis = -599:4200;
 
 % Get a list of all files and folders in the current directory
 files = dir('Comp*');
@@ -25,10 +27,6 @@ folderNames = folderNames(~ismember(folderNames, {'.', '..'}));
 disp('Folders in the current working directory:');
 disp(folderNames);
 
-% axes for plots: 
-% faxis=  0:1000/3200:125;  % for 601:2200
-% faxis=  0:1000/2800:125;  % for 801:2200
-% faxis=  0:1000/2400:125;  % for 1000:2200
 
 % loop pver subjects
 for subindex = 1:size(folderNames,2)
@@ -50,11 +48,17 @@ for subindex = 1:size(folderNames,2)
     fname3 = getfilesindir(pwd, '*.at3.ar');
     fname4 = getfilesindir(pwd, '*.at4.ar');
 
-    % do the hilbert transform
+    % do the hilbert transform at 6.666
     [demodmat1, ~]=steadyHilbert(fname1, 6.66, 500:600, 9, 0, []);
     [demodmat2, ~]=steadyHilbert(fname2, 6.66, 500:600, 9, 0, []);
     [demodmat3, ~]=steadyHilbert(fname3, 6.66, 500:600, 9, 0, []);
     [demodmat4, ~]=steadyHilbert(fname4, 6.66, 500:600, 9, 0, []);
+
+     % do the hilbert transform
+    [demodmat11, ~]=steadyHilbert(fname1, 15, 500:600, 9, 0, []);
+    [demodmat12, ~]=steadyHilbert(fname2, 15, 500:600, 9, 0, []);
+    [demodmat13, ~]=steadyHilbert(fname3, 15, 500:600, 9, 0, []);
+    [demodmat14, ~]=steadyHilbert(fname4, 15, 500:600, 9, 0, []);
 
     % Read at files
     at1 = ReadAvgFile(fname1);
@@ -91,7 +95,12 @@ for subindex = 1:size(folderNames,2)
     plot(taxis, demodmat1(137,:)), title(folderNames{subindex}), hold on
     plot(taxis, demodmat2(137,:))
     plot(taxis, demodmat3(137,:))
-    plot(taxis, demodmat4(137,:)), hold off
+    plot(taxis, demodmat4(137,:)), 
+    plot(taxis, demodmat11(137,:))
+    plot(taxis, demodmat12(137,:))
+    plot(taxis, demodmat13(137,:))
+    plot(taxis, demodmat14(137,:))
+    hold off
 
     artfname = getfilesindir(pwd, '*artiflog.mat');
     load(artfname)
