ed72c7fa7192e4125a8cbc2de0f6f53527cbca77
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2025-02-17 13:24:44 -0500

    Update OCA_RDKsinglesub_prepro.m

--- Libas/4scriptsandpipelines/OCA_RDKsinglesub_prepro.m
+++ Libas/4scriptsandpipelines/OCA_RDKsinglesub_prepro.m
@@ -2,9 +2,9 @@
 % Get a list of all files and folders in the current directory
 temp99 = eeglab; 
 
-cd '/Volumes/G-RAID Thunderbolt 3/OCA_project/OCA_RDK'
+% cd '/Volumes/G-RAID Thunderbolt 3/OCA_project/OCA_RDK'
 
-files = dir("RDK*");
+files = dir("OCA*");
 
 % Filter out the non-folder entries
 dirFlags = [files.isdir];
@@ -20,7 +20,7 @@ disp('Folders in the current working directory:');
 disp(folderNames);
 
 % loop over subjects
-for subindex = 30:size(folderNames,2)
+for subindex = 1:size(folderNames,2)
 
     eval(['cd ' folderNames{subindex}])
 
@@ -29,8 +29,8 @@ for subindex = 30:size(folderNames,2)
     datfile = getfilesindir(pwd, '*.dat');
     rawfile = getfilesindir(pwd, '*.RAW');
 
-    % actual preprocessing
-   LB3_prepro_pipeline(rawfile, datfile, 'getcon_COARD_RDK', 12, {'1' '2' '3' '4'}, [-.6 9], [3  30], [4 9], 1, 'GSN-HydroCel-256.sfp', 'HC1-256.ecfg', 1);
+  % actual preprocessing
+      LB3_prepro_pipeline(rawfile, datfile, 'getcon_COARD_RDK', 12, {'1' '2' '3' '4'}, [-.6 9], [3  30], [4 9], 1, 'GSN-HydroCel-256.sfp', 'HC1-256.ecfg', 1);
   %  LB3_prepro_pipeline(datapath, logpath, convecfun, stringlength, conditions2select, timevec, filtercoeffHz, filtord, skiptrials, sfpfilename, ecfgfilename, eyecorrflag)
 
     cd ..
