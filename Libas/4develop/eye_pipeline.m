b9a89a381f00ccaaa1aaf2ac2fd8c939675e4bce
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2025-02-19 17:21:23 -0500

    Update eye_pipeline.m

--- Libas/4develop/eye_pipeline.m
+++ Libas/4develop/eye_pipeline.m
@@ -1,4 +1,4 @@
-%%Function called by "Pupil_preproc.m" 
+
 %%inputs edf file, sample rate, condition vector function, condition file names, name of trigger, pre stimulus onset timepoints, post simulus onset timepoints)
 %%outputs: matcorr - timepoints by trials; matout - timepoints by condition; matoutbsl - matout baseline corrected, percentbad(-vec,-sub,-cond) - contains
     %%several stats on bad trials; avgcond - average over all trials per condition
