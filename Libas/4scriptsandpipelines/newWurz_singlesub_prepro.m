1cbed14439d88b1f6b9acb54f46d06649ad21713
A-Mirifar <128939712+A-Mirifar@users.noreply.github.com>
2025-03-05 10:57:50 -0500

    Update newWurz_singlesub_prepro.m

--- Libas/4scriptsandpipelines/newWurz_singlesub_prepro.m
+++ Libas/4scriptsandpipelines/newWurz_singlesub_prepro.m
@@ -1,7 +1,7 @@
 %% Script for analyzing New Wurzburg EEG data
 clear
 % Get a list of all files and folders in the current directory
-temp99 = eeglab; 
+% temp99 = eeglab; 
 
 cd '/Volumes/TOSHIBA EXT/New_Wurzburg/Data/one_event_marker/'
 
@@ -32,8 +32,8 @@ for subindex = 1:size(folderNames,2)
     datfile = deblank(tempmat(1,:));
 
     % actual preprocessing
-   prepro_scadsandspline_log(rawfile, datfile, 'getCon_NewWurz', 12, {'1' '2' '3' '4' '5' }, [-.8 7], [3  32], [4  9], 3, 'GSN-HydroCel-128.sfp', 'HC1-128.ecfg');
-   % prepro_scadsandspline_log(datapath, logpath, convecfun, stringlength, conditions2select, timevec, filtercoeffHz, filtord, skiptrials)
+   prepro_scadsandspline_log(rawfile, datfile, 'getCon_NewWurz', 12, {'1' '2' '3' '4' '5' }, [-.8 7], [3  32], [4  9], 1, 'GSN-HydroCel-128.sfp', 'HC1-128.ecfg');
+   % prepro_scadsandspline_log(datapath, logpath, convecfun, stringlength, conditions2select, timevec, filtercoeffHz, filtord, skiptrials, sfpfilename, ecfgfilename) 
     cd ..
 
     pause(.5)
