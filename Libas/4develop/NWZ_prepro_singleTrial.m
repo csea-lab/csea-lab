e0d5f7b19ed6b245750aca702dbf467ccb149b6b
hannahengle <hengle@ufl.edu>
2025-04-08 10:52:25 -0400

    Start of new_wurz single trial prepro for bayesian modeling

---
+++ Libas/4develop/NWZ_prepro_singleTrial.m
@@ -0,0 +1,43 @@
+%% Script for analyzing New Wurzburg EEG data
+clear
+% Get a list of all files and folders in the current directory
+temp99 = eeglab; 
+
+cd '/Volumes/TOSHIBA_EXT/New_Wurzburg/Data'
+
+files = dir("new_*");
+
+% Filter out the non-folder entries
+dirFlags = [files.isdir];
+
+% Extract the names of the folders
+folderNames = {files(dirFlags).name};
+
+% Remove the '.' and '..' folders
+folderNames = folderNames(~ismember(folderNames, {'.', '..'}));
+
+% Display the folder names
+disp('Folders in the current working directory:');
+disp(folderNames);
+
+% loop over subjects
+for subindex = 1:size(folderNames,2)
+
+    eval(['cd ' folderNames{subindex}])
+
+    % delete *.at*
+    
+    rawfile = getfilesindir(pwd, '*.RAW');
+    tempmat = getfilesindir(pwd, '*.csv');
+    datfile = deblank(tempmat(1,:));
+
+    % actual preprocessing
+   prepro_scadsandspline_log(rawfile, datfile, 'getCon_newWurz_singleTrial', 12, {'1' '2' '3' '4' '5'}, [-.8 7], [3  32], [4  9], 3, 'GSN-HydroCel-128.sfp', 'HC1-128.ecfg');
+   prepro_scadsandspline_log(rawfile, datfile, 'getCon_newWurz_singleTrial_2trigs', 12, {'11' '12' '13' '14' '15' }, [9 .8], [3  32], [4  9], 3, 'GSN-HydroCel-128.sfp', 'HC1-128.ecfg');
+   % prepro_scadsandspline_log(datapath, logpath, convecfun, stringlength, conditions2select, timevec, filtercoeffHz, filtord, skiptrials)
+    cd ..
+
+    pause(.5)
+    fclose('all');
+
+end
---
+++ Libas/4develop/getCon_newWurz_singleTrial.m
@@ -0,0 +1,8 @@
+function [conditionvec] = getCon_newWurz_singleTrial(filepath)
+%first, read in a new_wurz log file with RTs and everything
+table = readtable(filepath);
+conditionvec = table(:, 1);
+
+
+% if subject 226 and beyond, add a code for second event marker
+
