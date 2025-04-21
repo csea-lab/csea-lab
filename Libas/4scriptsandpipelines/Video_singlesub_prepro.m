285009b16c07b4c622e499e65b40f993008522f5
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2025-04-21 12:11:40 -0400

    Create Video_singlesub_prepro.m

---
+++ Libas/4scriptsandpipelines/Video_singlesub_prepro.m
@@ -0,0 +1,39 @@
+%% Script for analyzing all video data
+% Get a list of all files and folders in the current directory
+temp99 = eeglab; 
+
+files = dir("emo*");
+
+% Filter out the non-folder entries
+dirFlags = [files.isdir];
+
+% Extract the names of the folders
+folderNames = {files(dirFlags).name};
+
+% Remove the '.' and '..' folders
+folderNames = folderNames(~ismember(folderNames, {'.', '..'}));
+
+% Display the folder names
+disp('Folders in the current working directory:');
+disp(folderNames);
+
+% loop over subjects
+for subindex = 1:size(folderNames,2)
+
+    eval(['cd ' folderNames{subindex} '/EEG/run1'])
+
+   delete *.at*
+
+    datfile = getfilesindir(pwd, '*.csv');
+    rawfile = getfilesindir(pwd, '*.RAW');
+
+    % actual preprocessing
+   LB3_prepro_pipeline(rawfile, datfile, 'getcon_video', 6, {'1' '2' '3' }, [-.6 10], [3  30], [4 9], 1, 'GSN-HydroCel-256.sfp', 'HC1-256.ecfg', 1, [])
+   % prepro_scadsandspline(datapath, logpath, convecfun, stringlength, conditions2select, timevec, filtercoeffHz, filtord, skiptrials)
+
+    cd ..
+
+    pause(.5)
+    fclose('all');
+
+end
