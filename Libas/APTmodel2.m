613fcb6996a4813f3c6f95ed6d28b9c6dfb6d1be
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2025-06-05 15:04:45 -0400

    Update APTmodel2.m

--- Libas/APTmodel2.m
+++ Libas/APTmodel2.m
@@ -2,13 +2,10 @@ function [Rvec, anterior_emotional,  A, R] = APTmodel2(params, reinforcement)
  
 BETA =  params(1); 
 LAG = params(2); 
-adaptation = params(3); 
 
 stimnum = 7; % pick an odd number so that CS+ can be in the middle
 
 [w, delta] = rescorlaWagnerLearnMod([BETA 1], reinforcement'); % execute rescorla wagner model, get assoc. strength and pred error
-
-delta_smooth = movmean(delta, LAG); 
 
 for index = 1:length(reinforcement)
     
@@ -22,13 +19,13 @@ for index = 1:length(reinforcement)
     
     VM = shiftLam(latvec)'; % lateral inhibition matrix
 
-    input = ones(1,stimnum)./10+gaussPro(-(stimnum-1)/2:(stimnum-1)/2, 1./(w(index)/delta_smooth(index))) *sign(delta_smooth(index)) *w(index); % generalization to surrounding stimuli, width changes with assoc strength, magnitide with prediction error
+    input = ones(1,stimnum)./10+gaussPro(-(stimnum-1)/2:(stimnum-1)/2, 1./(w(index)/delta(index))) *(delta(index)) *w(index); % generalization to surrounding stimuli, width changes with assoc strength, magnitide with prediction error
 
-  %  input = ones(1,stimnum)./10+gaussPro(-(stimnum-1)/2:(stimnum-1)/2, 1./(w(index).*3)) *sign(delta_smooth(index)) *w(index); % generalization to surrounding stimuli, width changes with assoc strength, magnitide with prediction error
+  %  input = ones(1,stimnum)./10+gaussPro(-(stimnum-1)/2:(stimnum-1)/2, 1./(w(index).*3)) *sign(delta(index)) *w(index); % generalization to surrounding stimuli, width changes with assoc strength, magnitide with prediction error
 
-    anterior_emotional(:, index) = gaussPro(-(stimnum-1)/2:(stimnum-1)/2, 1./(w(index).*3)) *(delta_smooth(index)); 
+    anterior_emotional(:, index) = gaussPro(-(stimnum-1)/2:(stimnum-1)/2, 1./(w(index).*3)) *(delta(index)); 
     
-   % amygdala(:, index) = gaussPro(-(stimnum-1)/2:(stimnum-1)/2, 1./(w(index).*3)) *(delta_smooth(index)); 
+   % amygdala(:, index) = gaussPro(-(stimnum-1)/2:(stimnum-1)/2, 1./(w(index).*3)) *(delta(index)); 
     
     A(:, index) = input'; 
     
@@ -38,22 +35,11 @@ for index = 1:length(reinforcement)
 
 end
 
-% apply adaption to the R
-x = 0:length(reinforcement)-1; 
-regline = x*adaptation; 
+anterior_emotional(:, 1:5) = 0; 
 
-for rowindex = 1:size(R, 1)
-    R(rowindex, :) = R(rowindex, :) - regline; 
-end
-
-
-
-
-%anterior_emotional(:, 1:5) = 0; 
- % 
- % figure(10), 
+ % figure, 
  % subplot(3,1,3), colormap('jet'), contourf(R, 12), title('visual cortex'), caxis([max(max(R)).*-1 max(max(R))]); colorbar
  % subplot(3,1,2),colormap('jet'), contourf(A, 12), title('attention systems'),caxis([max(max(A)).*-1 max(max(A))]); colorbar
  % subplot(3,1,1),colormap('jet'), contourf(anterior_emotional, 12), title('anterior emotion-modulated structures'), caxis([max(max(anterior_emotional)).*-1 max(max(anterior_emotional))]); colorbar
 
-Rvec = mat2vec(R(4:end,:)')'; 
+Rvec = movmean(mat2vec(R(4:end,:)')', LAG); 
