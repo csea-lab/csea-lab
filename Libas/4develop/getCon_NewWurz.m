4777e397f367e277de3d728860141423c8705b2b
hannahengle <hengle@ufl.edu>
2025-06-06 15:53:03 -0400

    adding function to loop condispa participants to make single trial data frama
    also updated new wurz con file to include the trial number of participants with missing event markers

--- Libas/4develop/getCon_NewWurz.m
+++ Libas/4develop/getCon_NewWurz.m
@@ -39,4 +39,19 @@ for row = 1:size(table,1)
     end
 end
 
+% version for subjects with missing event markers 
+missingTrials = containers.Map;
+    missingTrials('New_Wurz_201.csv') = [1, 59, 212];
+    missingTrials('New_Wurz_202.csv') = [165];
+    missingTrials('New_Wurz_209.csv') = [132];
+    missingTrials('New_Wurz_211.csv') = [46, 101];
+    missingTrials('New_Wurz_213.csv') = [77];
+
+
+% Check if this subject has missing trials
+    if isKey(missingTrials, filepath)
+        indices = missingTrials(filepath);
+        conditionvec(indices) = [];
+    end
+
 
