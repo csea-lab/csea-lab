a2cff5f42d45b544672010308e9d4fd55c3258c8
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2025-07-29 15:27:55 -0400

    Update modweibullm.m

--- Libas/modweibullm.m
+++ Libas/modweibullm.m
@@ -2,29 +2,30 @@ function [parameters, mse] = modweibullm(M, grandmean, plotflag)
 
 % M = n by 36 (trial) matric from alexandroperant experiment
 
-option = statset('MaxIter', 20000, 'TolFun', 0.01,  'Robust', 'off');
+option = statset('MaxIter', 20000, 'TolFun', 0.01,  'Robust', 'off'); % these are the options fro nonlinfit
 xgrid = 1:length(M(1,:));
 
-for subject = 1:size(M,1); 
+for subject = 1:size(M,1)
 
-disp(subject)
+ clf
 
-ind_slope =  find(abs(diff(M(subject,:)) > .2))
+disp('subject: '), disp(subject)
 
-data4fit = (M(subject,:)' + 5.*grandmean')./6; 
-%data4fit = (M(subject,:))';
+data4fit = (M(subject, :) + grandmean)./2; 
+
+data4fit = data4fit./max(data4fit); % !!!
 
  if plotflag
-     hold on, plot(xgrid, data4fit, ['k']), hold off    
+     hold on, plot(xgrid, data4fit, 'k'), hold off    
  end
 
-[paras,r(subject,:),J,Sigma,mse(subject,:)]  = nlinfit(column(0:35), data4fit, @weibull, [8 .8 .5 -.1]', option);
+[paras,r(subject,:),J,Sigma,mse(subject,:)]  = nlinfit(column(0:35), column(data4fit), @weibull, [6 .8 .5 -.1]', option);
 parameters(subject,:) = real(paras);
-real(paras)
+real(paras); 
 
     if plotflag
     line(xgrid, weibull(parameters(subject,:), xgrid), 'Color', 'r'); title(num2str(subject))
-    pause(.5), clf
+    pause(.5)
     end
 
 end
