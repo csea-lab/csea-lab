299eeff6e2bc4edc9b0cacaf8917738fa5943791
akeilCSEA <akeil@ufl.edu>
2025-08-21 18:12:55 -0400

    updated

--- Libas/4develop/gaborgen_trialalpha_ak.m
+++ Libas/4develop/gaborgen_trialalpha_ak.m
@@ -1,12 +1,37 @@
-function [] = gaborgen_trialalpha_ak(EEGmatfile)
+function [diffampOz, meandiff] = gaborgen_trialalpha_ak(subjectID)
 
-tmp = load(EEGmatfile); 
-data = tmp.EEG.data;
+meandiff = zeros(1,8);
+conditionvec = [11:14 21:24];
 
-figure(101); 
-subplot(4,1,1), plot(tmp.EEG.times, squeeze(data(20, :, :)))
+for conditionindex = 1:8
 
-[ampout3d1, phaseout3d, freqs] = FFT_spectrum3D_singtrial(data, 361:961, 600, 1:100);
-[ampout3d2, phaseout3d, freqs] = FFT_spectrum3D_singtrial(data, 961:1561, 600, 1:100);
+    EEGmatfile = [num2str(subjectID) '_' num2str(conditionvec(conditionindex)) '_NEW_EEG.mat'];
 
-subplot(4,1,2), plot(freqs(1:50), squeeze(ampout3d2(20, 1:50, :)))
+    tmp = load(EEGmatfile);
+    data = tmp.EEG.data;
+
+    [ data, badindex, NGoodtrials ] = scadsAK_3dtrials(data, 2); 
+
+    badindex, pause
+
+    figure(101);
+    subplot(4,1,1), plot(tmp.EEG.times, squeeze(data(20, :, :))), axis([-1600 2000 min(min(data(20, :, :))) max(max(data(20, :, :)))]);
+    title(['condition ' num2str(conditionvec(conditionindex)) ': voltage at Oz'])
+
+    [ampout3d1, ~, freqs] = FFT_spectrum3D_singtrial(data, 361:961, 600, 1:100);
+    [ampout3d2, ~] = FFT_spectrum3D_singtrial(data, 1161:1761, 600, 1:100);
+
+    subplot(4,1,2), plot(freqs(1:50), squeeze(ampout3d1(20, 1:50, :))), title('baseline spectrum')
+    subplot(4,1,3), plot(freqs(1:50), squeeze(ampout3d2(20, 1:50, :))), title('spectrum during Gabor')
+
+    diffampOz= ampout3d2-ampout3d1; % figure, plot(freqs(1:50), squeeze(diffamp(20, 1:50, :)))
+
+    subplot(4,1,4), bar(squeeze(mean(diffampOz(20, 9:14, :), 2))), title(['alpha power change from baseline'])
+
+    meandiff(conditionindex) = mean(squeeze(mean(diffampOz(20, 9:14, :), 2)));
+
+    pause
+
+end
+
+  figure(102), bar(meandiff), pause
