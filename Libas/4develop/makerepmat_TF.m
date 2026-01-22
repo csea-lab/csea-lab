63793e01599f79c272ec82c754d7d17cb58d6e8d
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2026-01-22 16:45:18 -0500

    Update makerepmat_TF.m

--- Libas/4develop/makerepmat_TF.m
+++ Libas/4develop/makerepmat_TF.m
@@ -1,10 +1,10 @@
-function [repmat] = makerepmat_TF(filemat, Nsubjects, Nconditions, freqindex, optionalbaseline)
+function [repmat] = makerepmat_TF(filemat, Nsubjects, electrode, Nconditions, optionalbaseline)
 
 % makes  4-D array for contrast_rep 
 % if each person/condition file has 3-D 
 % (elec by time by freq)
-% this selects one frequency and uses the time course of that frequency for
-% the repmat
+
+% the repmat has only one electrode
 
 for con = 1:Nconditions
     
@@ -23,7 +23,7 @@ for con = 1:Nconditions
       
 
       
-            repmat(:, :, sub, con) = squeeze(mat(:, :, freqindex)); 
+            repmat(:, :, sub, con) = squeeze(mat(electrode, :, :)); 
       
 
        
