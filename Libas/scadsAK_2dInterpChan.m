e5bbf1a1d1cfa538ecff2f4b3b1c1373162e3362
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2026-07-28 15:11:42 -0400

    Update scadsAK_2dInterpChan.m

--- Libas/scadsAK_2dInterpChan.m
+++ Libas/scadsAK_2dInterpChan.m
@@ -25,7 +25,7 @@ function [outmat2d, interpvec] = scadsAK_2dInterpChan(inmat2d, locations, thresh
    
     % calculate compound quality index
     qualindex = absvalvec+ stdvalvec+ maxtransvalvec; 
-    figure(101)
+    figure(101), title('bad channel QC, 2D')
     plot(qualindex)
     
     
