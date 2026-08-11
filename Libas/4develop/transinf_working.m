c58ac12fac29b87fff1fbe776b57d1d750b87b1d
SarahMarieG <135059341+SarahMarieG@users.noreply.github.com>
2026-08-11 10:24:45 -0400

    Added similarity ratings code

--- Libas/4develop/transinf_working.m
+++ Libas/4develop/transinf_working.m
@@ -149,6 +149,11 @@ HideCursor(win)
 datafilename1 = [dataDir 'tinfLog1s' num2str(subjectNumber) '.dat'];
 datafilepointer1 = fopen(datafilename1, 'w');
 
+% log file define and open for ratings 1
+datafileratings1 = [dataDir 'tinfLogrates' num2str(subjectNumber) '.dat'];
+datafilepointerrate1 = fopen(datafileratings1);
+
+% log file define and open for Learning 1
 datafilename2 = [dataDir 'tinfLog2s' num2str(subjectNumber) '.dat'];
 datafilepointer2 = fopen(datafilename2, 'w');
 
@@ -221,6 +226,7 @@ for trialindex_bsl1 = 1:NTrials-49
 end
 
 %% 6 similarity rating
+   
 
 
 %% 7 learning Phase 
@@ -237,13 +243,11 @@ SetMouse(cx, cy, win);   % park cursor at centre between trials
 
 % randomization
 NTrials = (nCategories-1)*nImgsPerCat/2; % trials in baseline phase = all pictures
-
 counterA = 0;
 counterB = 0;
 counterC = 0;
 counterD = 0;
 counterE = 0;
-
 pairlabelvec = [];
 switchsidevec = []; 
 for loopindex = 1:nImgsPerCat/2
@@ -255,7 +259,7 @@ switchsidevec = [switchsidevec fliplr(switchsidevec)];
 
 for trialindex_training = 1:NTrials
     % first, set the event marker channel to zero
-    fprintf(s3, '00');
+    % fprintf(s3, '00');
     
     %Select the stimulus pair    
     if pairlabelvec(trialindex_training) == 1
@@ -364,6 +368,186 @@ Screen('Flip', win);
 WaitSecs(.2);
 % Close the window and exit
 Screen('CloseAll');
-%%
 
 end % main function transinf end
+
+
+
+%% Similarity Ratings
+
+function similarityRatings(win, roles, subjectNumber, phaseLabel)
+
+winRect = Screen('Rect', win);
+[cx, cy] = RectCenter(winRect);
+scrW = RectWidth(winRect);
+scrH = RectHeight(winRect);
+
+% --- image display rects, flanking center ---
+imgW = round(scrW * 0.30);
+imgH = round(scrH * 0.55);
+xOffset = round(scrW * 0.25);
+
+leftRect  = CenterRectOnPointd([0 0 imgW imgH], cx - xOffset, cy);
+rightRect = CenterRectOnPointd([0 0 imgW imgH], cx + xOffset, cy);
+
+% --- fixation dot ---
+fix_cord = [cx-5 cy-5 cx+5 cy+5];
+
+% --- slider setup (adapted from simrate2) ---
+locationPosFrac = 0:0.1:1;              % 11 selectable positions, 0 to 1
+locationPosTick = [0 0.5 1];             % only these get drawn/labeled
+sliderWidth   = scrW * 0.6;
+sliderHeight  = 20;
+sliderXcenter = scrW / 2;
+sliderY       = scrH * 0.85;
+sliderRect    = CenterRectOnPointd([0 0 sliderWidth sliderHeight], sliderXcenter, sliderY);
+sliderBgColor  = [100 100 100];
+locMarkerColor = [0 255 0];
+
+% randomization
+NTrials = (nCategories-1)*nImgsPerCat/2; % trials in baseline phase = all pictures
+
+counterA = 0;
+counterB = 0;
+counterC = 0;
+counterD = 0;
+counterE = 0;
+
+pairlabelvec = [];
+switchsidevec = [];
+
+for loopindex = 1:nImgsPerCat/2
+    pairlabelvec = [pairlabelvec randperm(4)];
+    switchsidevec = [switchsidevec randperm(2)];
+end
+
+switchsidevec = [switchsidevec fliplr(switchsidevec)];
+
+for trialindex_training = 1:NTrials
+    % first, set the event marker channel to zero
+    fprintf(s3, '00');
+
+    %Select the stimulus pair
+    if pairlabelvec(trialindex_training) == 1
+        counterA = counterA+1;
+        TrialPicturePath1 =  eval(['roles.A.imgPaths{' num2str(counterA) '}']);
+        counterB = counterB+1;
+        TrialPicturePath2 =  eval(['roles.B.imgPaths{' num2str(counterB) '}']);
+    elseif pairlabelvec(trialindex_training) == 2
+        counterB = counterB+1;
+        TrialPicturePath1 =  eval(['roles.B.imgPaths{' num2str(counterB) '}']);
+        counterC = counterC+1;
+        TrialPicturePath2 =  eval(['roles.C.imgPaths{' num2str(counterC) '}']);
+    elseif pairlabelvec(trialindex_training) == 3
+        counterC = counterC+1;
+        TrialPicturePath1 =  eval(['roles.C.imgPaths{' num2str(counterC) '}']);
+        counterD = counterD+1;
+        TrialPicturePath2 =  eval(['roles.D.imgPaths{' num2str(counterD) '}']);
+    elseif pairlabelvec(trialindex_training) == 4
+        counterD = counterD+1;
+        TrialPicturePath1 =  eval(['roles.D.imgPaths{' num2str(counterD) '}']);
+        counterE = counterE+1;
+        TrialPicturePath2 =  eval(['roles.E.imgPaths{' num2str(counterE) '}']);
+    end
+
+    % make the corresponding textures
+    [~, currentfile1] = fileparts(TrialPicturePath1);
+    [~, currentfile2] = fileparts(TrialPicturePath2);
+
+    TrialPicture1 = imread(TrialPicturePath1);
+    TrialPicture1 = imresize(TrialPicture1, .5);
+    TrialTex1=Screen('MakeTexture', win, TrialPicture1);
+
+    TrialPicture2 = imread(TrialPicturePath2);
+    TrialPicture2 = imresize(TrialPicture2, .5);
+    TrialTex2=Screen('MakeTexture', win, TrialPicture2);
+
+    % --- response phase: images + slider together, wait for click ---
+    ShowCursor('Arrow', win);
+    responseMade = false;
+    tStart = GetSecs;
+
+    while ~responseMade
+        [mx, ~, buttons] = GetMouse(win);
+
+        % constrain mouse x to the slider's range
+        if mx < sliderXcenter - sliderWidth/2, mx = sliderXcenter - sliderWidth/2; end
+        if mx > sliderXcenter + sliderWidth/2, mx = sliderXcenter + sliderWidth/2; end
+        fillWidth = mx - (sliderXcenter - sliderWidth/2);
+
+        if strcmp(idx1side, 'left')
+            Screen('DrawTexture', win, TrialTex1, [], leftRect);
+            Screen('DrawTexture', win, TrialTex2, [], rightRect);
+        else
+            Screen('DrawTexture', win, TrialTex1, [], rightRect);
+            Screen('DrawTexture', win, TrialTex2, [], leftRect);
+        end
+
+        Screen('FillOval', win, 255, fix_cord);
+        DrawFormattedText(win, 'How similar are these in meaning?', 'center', cy - 250, 255);
+
+        drawSimSlider(win, sliderRect, sliderBgColor, locationPosTick, ...
+            sliderWidth, sliderHeight, sliderXcenter, sliderY, locMarkerColor, fillWidth);
+
+        Screen('Flip', win);
+
+        if any(buttons)
+            sliderLeft = sliderXcenter - sliderWidth/2;
+            normPos = (mx - sliderLeft) / sliderWidth;
+            [~, choiceLoc] = min(abs(locationPosFrac - normPos));
+            simresponse = choiceLoc - 1;     % 0-10 scale
+            responsecont = mx;
+            thisRT = GetSecs - tStart;
+            responseMade = true;
+        end
+
+        WaitSecs(0.03);
+    end
+
+    HideCursor(win);
+    Screen('Close', [TrialTex1 TrialTex2]);   % release these two textures now that the trial is done
+
+
+    % --- log this trial ---
+    fprintf(datfilepointer, '%i %i %i %s %s %s %i %i %.4f \n', ...
+        subjectNumber, ...
+        str2double(phaseLabel), ...
+        trialindex_sim, ...
+        currentfile1, ...
+        currentfile2, ...
+        idx1side, ...
+        simresponse, ...
+        responsecont, ...
+        thisRT);
+end
+end
+
+
+%%
+function drawSimSlider(win, sliderRect, bgColor, locationPosTick, ...
+    sliderWidth, sliderHeight, sliderXcenter, sliderY, locColor, fillWidth)
+
+% slider background bar
+Screen('FillRect', win, bgColor, sliderRect);
+
+% red fill tracking the mouse position
+fillWidth = max(0, fillWidth);
+fillRect = [sliderRect(1) sliderRect(2) sliderRect(1)+fillWidth sliderRect(4)];
+Screen('FillRect', win, [255 0 0], fillRect);
+
+% green tick markers at 0%, 50%, 100%
+sliderLeft = sliderXcenter - sliderWidth/2;
+for i = 1:length(locationPosTick)
+    xPos = sliderLeft + sliderWidth * locationPosTick(i);
+    lineRect = [xPos-1 sliderY-30 xPos+1 sliderY+sliderHeight+15];
+    Screen('FillRect', win, locColor, lineRect);
+end
+
+% labels under the 0%/50%/100% ticks
+labels = {'0% similar', '', '100% similar'};
+for i = 1:length(locationPosTick)
+    xPos = sliderLeft + sliderWidth * locationPosTick(i);
+    DrawFormattedText(win, labels{i}, round(xPos)-60, sliderY+sliderHeight+50, locColor);
+end
+
+end
